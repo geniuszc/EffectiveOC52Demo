bac26a002984ef3e5320edf54649f2ef65613b99
geniuszc <170393592@qq.com>
2015-04-15 10:36:59 +0800

    Initial Commit

---
+++ EffectiveOC52Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EffectiveOC52Demo
+//
+//  Created by geniuszc on 15/4/15.
+//  Copyright (c) 2015年 Mattservice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EffectiveOC52Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EffectiveOC52Demo
+//
+//  Created by geniuszc on 15/4/15.
+//  Copyright (c) 2015年 Mattservice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
